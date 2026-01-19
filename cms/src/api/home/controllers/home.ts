121b629ef3dab48fa73e91a0a4eb9b5631305421
Ysato <y.sugar0402@gmail.com>
2026-01-19 22:02:12 +0900

    banner fix

--- cms/src/api/home/controllers/home.ts
+++ cms/src/api/home/controllers/home.ts
@@ -4,36 +4,45 @@
 
 import { factories } from '@strapi/strapi';
 
+const applyPopulate = (ctx: any) => {
+  ctx.query = {
+    ...ctx.query,
+    populate: {
+      pickupMediums: {
+        populate: {
+          coverImage: true,
+        },
+      },
+      popularItems: {
+        populate: {
+          coverImage: true,
+        },
+      },
+      featuredItems: {
+        populate: {
+          coverImage: true,
+        },
+      },
+      banners: {
+        populate: {
+          desktopImage: true,
+          mobileImage: true,
+        },
+      },
+      lineCta: true,
+    },
+  };
+};
+
 export default factories.createCoreController('api::home.home', () => ({
   async find(ctx) {
-    ctx.query = {
-      ...ctx.query,
-      populate: {
-        pickupMediums: {
-          populate: {
-            coverImage: true,
-          },
-        },
-        popularItems: {
-          populate: {
-            coverImage: true,
-          },
-        },
-        featuredItems: {
-          populate: {
-            coverImage: true,
-          },
-        },
-        banners: {
-          populate: {
-            desktopImage: true,
-            mobileImage: true,
-          },
-        },
-        lineCta: true,
-      },
-    };
+    applyPopulate(ctx);
 
     return await super.find(ctx);
+  },
+  async findOne(ctx) {
+    applyPopulate(ctx);
+
+    return await super.findOne(ctx);
   },
 }));
